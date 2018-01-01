09db2fee5f8dc019f813996e622aeff0c3d31340
xwx <291857648@qq.com>
2018-01-01 21:46:30 +0800

    first

--- js/main.js
+++ js/main.js
@@ -1,20 +1,22 @@
 var index = 0;
 var imgWidth = 400;
-isGo = false;
+var isGo = false;
+var imgWidth = 400;
+var imgLength = $(".imgbox img").length
 $(".next").on('click',function(){
 	if(isGo){
 		return
 	}
 	isGo = true
 
-	var nowLeft =  parseInt($(".imgbox").css('margin-left'))-400
+	var nowLeft =  offset(-imgWidth)
 	$(".imgbox").animate({"margin-left":nowLeft},function(){
 		index ++;
-		if(parseInt($(".imgbox").css('margin-left'))<=-1600){
+		if(margin()==-(imgLength-1)*imgWidth){
 			index = 0
-			$(".imgbox").css("margin-left","-400px")
+			$(".imgbox").css("margin-left",""+(-imgWidth)+"px")
 		}
-		$(".dot > span").eq(index).addClass('active').siblings().removeClass('active')
+		setDOt()
 		isGo = false
 	})
 })
@@ -24,42 +26,60 @@ $(".prev").on('click',function(){
 	}
 	isGo = true
 
-	var nowLeft =  parseInt($(".imgbox").css('margin-left'))+400
+	var nowLeft =  offset(imgWidth)
 	$(".imgbox").animate({"margin-left":nowLeft},function(){
 		index --;
-		if(parseInt($(".imgbox").css('margin-left'))>-400){
-			index = 2
-			$(".imgbox").css("margin-left","-1200px")
+		if(margin()==-0){
+			index = imgLength-3
+			$(".imgbox").css("margin-left",""+(-(imgLength-2)*imgWidth)+"px")
 		}
-		$(".dot > span").eq(index).addClass('active').siblings().removeClass('active')
+		setDOt()
 		isGo = false
 	})
 })
-
+//设置便宜距离
+function offset(imgWidth){
+	var nowLeft =  margin()+imgWidth;
+	return nowLeft;
+}
+//获取当前距离margin-left距离
+function margin(){
+	return parseInt($(".imgbox").css('margin-left'))
+}
+//设置原点高亮
+function setDOt(){
+	$(".dot > span").eq(index).addClass('active').siblings().removeClass('active')
+}
 
 //原点事件
 $(".dot > span").on('click',function(){
+	clearInterval(timeId)
 	var nowIndex = $(this).index();
 	var offsetIndex = (nowIndex - index)
 	if(!offsetIndex){
 		return;
 	}
-	oldOffset = parseInt($(".imgbox").css('margin-left')) - offsetIndex*400
+	oldOffset = margin() - offsetIndex*imgWidth
 	$(this).addClass('active').siblings().removeClass('active')
 	$(".imgbox").animate({'margin-left':oldOffset})
 	index = nowIndex
 })
 
-var timeId = setInterval(()=>{
-	$(".next").trigger('click')
-},1000)
+var timeId = timeSet()
 
-$(".imgbox,.next,.prev").on('mouseenter',function(){
-	clearInterval(timeId)
-})
-$(".imgbox,.next,.prev").on('mouseleave',function(){
-	timeId = setInterval(()=>{
+
+function timeSet(){
+	return	setInterval(()=>{
 		$(".next").trigger('click')
 	},1000)
+}
+
+
+$(".imgbox,.next,.prev,.dot").on('mouseenter',function(){
+	clearInterval(timeId)
+})
+$(".imgbox,.next,.prev,.dot").on('mouseleave',function(){
+	clearInterval(timeId)
+	timeId = timeSet()
 })
 
