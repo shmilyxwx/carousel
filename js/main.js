0a86f37a0dc3785586d7d5534719a1b9f1d4bbf3
xwx <291857648@qq.com>
2018-01-03 10:55:02 +0800

    carousel

--- js/main.js
+++ js/main.js
@@ -9,14 +9,14 @@ $(".next").on('click',function(){
 
 	isGo = true
 
-	var nowLeft =  carousel.offset(-imgWidth)
+	var nowLeft =  offset(-imgWidth)
 	$(".imgbox").animate({"margin-left":nowLeft},function(){
 		index ++;
-		if(carousel.margin()==-(imgLength-1)*imgWidth){
+		if(margin()==-(imgLength-1)*imgWidth){
 			index = 0
-			carousel.reset(-imgWidth)
+			reset(-imgWidth)
 		}
-		carousel.setDOt()
+		setDOt()
 		isGo = false
 	})
 })
@@ -26,45 +26,35 @@ $(".prev").on('click',function(){
 	
 	isGo = true
 
-	var nowLeft =  carousel.offset(imgWidth)
+	var nowLeft =  offset(imgWidth)
 	$(".imgbox").animate({"margin-left":nowLeft},function(){
 		index --;
-		if(carousel.margin()==-0){
+		if(margin()==-0){
 			index = imgLength-3
-			carousel.reset(-(imgLength-2)*imgWidth)
+			reset(-(imgLength-2)*imgWidth)
 		}
-		carousel.setDOt()
+		setDOt()
 		isGo = false
 	})
 })
-
-var carousel = {
-	//临界条件重置位置
-	reset(distance){
-		$(".imgbox").css("margin-left",distance+"px")
-	},
-		
-	//设置便宜距离
-	offset(imgWidth){
-		var nowLeft =  this.margin()+imgWidth;
-		return nowLeft;
-	},
-	//获取当前距离margin-left距离
-	margin(){
-		return parseInt($(".imgbox").css('margin-left'))
-	},
-	//设置原点高亮
-	setDOt(){
-		$(".dot > span").eq(index).addClass('active').siblings().removeClass('active')
-	},
-	//定时器轮播
-	timeSet(){
-		return	setInterval(()=>{
-			$(".next").trigger('click')
-		},1000)
-	}
+//临界条件重置位置
+function reset(distance){
+	$(".imgbox").css("margin-left",distance+"px")
 }
-
+	
+//设置便宜距离
+function offset(imgWidth){
+	var nowLeft =  margin()+imgWidth;
+	return nowLeft;
+}
+//获取当前距离margin-left距离
+function margin(){
+	return parseInt($(".imgbox").css('margin-left'))
+}
+//设置原点高亮
+function setDOt(){
+	$(".dot > span").eq(index).addClass('active').siblings().removeClass('active')
+}
 
 //原点事件
 $(".dot > span").on('click',function(){
@@ -74,15 +64,20 @@ $(".dot > span").on('click',function(){
 	if(!offsetIndex){
 		return;
 	}
-	oldOffset = carousel.margin() - offsetIndex*imgWidth
+	oldOffset = margin() - offsetIndex*imgWidth
 	$(".imgbox").animate({'margin-left':oldOffset})
 	index = nowIndex
-	carousel.setDOt()
+	setDOt()
 })
 
-var timeId = carousel.timeSet()
+var timeId = timeSet()
 
 
+function timeSet(){
+	return	setInterval(()=>{
+		$(".next").trigger('click')
+	},1000)
+}
 
 
 $(".imgbox,.next,.prev,.dot").on('mouseenter',function(){
@@ -90,6 +85,6 @@ $(".imgbox,.next,.prev,.dot").on('mouseenter',function(){
 })
 $(".imgbox,.next,.prev,.dot").on('mouseleave',function(){
 	clearInterval(timeId)
-	timeId = carousel.timeSet()
+	timeId = timeSet()
 })
 
